ac84536ab23ddef859bfebfe8b75987fc2302377
donggeonPark <selios2000@naver.com>
2017-04-20 22:17:37 +0900

    New changelist

---
+++ app/src/main/java/samsung/sec/com/isthere/MapsActivity.java
@@ -0,0 +1,79 @@
+package samsung.sec.com.isthere;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //37.491, 127.020
+        LatLng currentLocation = new LatLng(37.491, 127.020);
+        //마커를 원하는 이미지로 변경해줘야함
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(currentLocation);
+        markerOptions.title("BlueHack Hackathon");
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        //currentMarker = mGoogleMap.addMarker(markerOptions);
+        //37.2512358
+        //127.0187548
+        mMap.addMarker(markerOptions);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+        addTestmarker();
+        return;
+    }
+
+    public void addTestmarker(){
+        LatLng currentLocation1 = new LatLng(37.490072, 127.019828);
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(currentLocation1);
+        // markerOptions1.title("GS25");
+        markerOptions1.icon(BitmapDescriptorFactory.fromResource(R.drawable.gs25));
+
+        LatLng currentLocation2 = new LatLng(37.490140, 127.021223);
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(currentLocation2);
+        //markerOptions2.title("SevenEleven");
+        markerOptions2.icon(BitmapDescriptorFactory.fromResource(R.drawable.seveneleven));
+        mMap.addMarker(markerOptions1);
+        mMap.addMarker(markerOptions2);
+
+    }
+}
